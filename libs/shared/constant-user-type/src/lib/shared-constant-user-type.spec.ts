6d848592295bcb1ceba2f23802d9481a9a1dca8c
Subu <subramanya4shenoy@gmail.com>
2022-08-06 02:25:05 +0530

    Shared constants Unit test cases (#6)

--- libs/shared/constant-user-type/src/lib/shared-constant-user-type.spec.ts
+++ libs/shared/constant-user-type/src/lib/shared-constant-user-type.spec.ts
@@ -1,7 +1,22 @@
-import { sharedConstantUserType } from './shared-constant-user-type';
+import { ACCOUNT_TYPES, ACCOUNT_SETTINGS } from './shared-constant-user-type';
 
-describe('sharedConstantUserType', () => {
-    it('should work', () => {
-        expect(sharedConstantUserType()).toEqual('shared-constant-user-type');
-    })
+describe('Unit tests for Constant files', () => {
+   it("Should return types of end users", () => {
+    const acTypes  = ACCOUNT_TYPES;
+    expect(acTypes.length).toBe(2);
+   })
+
+   it("Should return proper values for Flenzr", () => {
+    const flenzr  = ACCOUNT_SETTINGS.Flenzr;
+    expect(flenzr.title).toBe('Flenzr');
+    expect(flenzr.id).toBe('Flenzr');
+    expect(flenzr.desc).toBe('');
+   })
+
+   it("Should return proper values for Brand", () => {
+    const brand  = ACCOUNT_SETTINGS.Brand;
+    expect(brand.title).toBe('Brand');
+    expect(brand.id).toBe('Brand');
+    expect(brand.desc).toBe('');
+   })
 })
