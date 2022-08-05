943e405626935eaf6818b8447ebe458441af784b
Subu <subramanya4shenoy@gmail.com>
2022-08-06 02:10:46 +0530

    Unit test case for Product page (#5)

--- libs/feature/product-page/jest.config.ts
+++ libs/feature/product-page/jest.config.ts
@@ -7,4 +7,8 @@ export default {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../coverage/libs/feature/product-page',
+  moduleNameMapper: {
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "../mocks/fileMock.js",
+    "\\.(css|less)$": "../mocks/fileMock.js"
+  }
 };
--- libs/feature/product-page/src/lib/feature-product-page.spec.tsx
+++ libs/feature/product-page/src/lib/feature-product-page.spec.tsx
@@ -1,10 +1,69 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from "@testing-library/react";
+import "jest-canvas-mock";
+import { useTranslation } from "react-i18next";
+import FeatureProductPage from "./feature-product-page";
 
-import FeatureProductPage from './feature-product-page';
+// Mocks used by the components
+jest.mock("react-i18next", () => ({
+  useTranslation: () => {
+    return {
+      t: (str: string) => str,
+      i18n: {
+        changeLanguage: () =>
+          new Promise(() => {
+            return;
+          }),
+      },
+    };
+  },
+}));
+const mockNavigateTo = jest.fn();
 
-describe('FeatureProductPage', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<FeatureProductPage />);
-    expect(baseElement).toBeTruthy();
+describe("Test cases for FeatureProductPage", () => {
+
+  /** Unit test cases to test the UI Elements */
+  describe("Test cases for FeatureProductPage UI", () => {
+    it("should render FeatureProductPage successfully", async () => {
+      const { baseElement } = render(
+        <FeatureProductPage navigateTo={mockNavigateTo} />
+      );
+      expect(baseElement).toBeTruthy();
+    });
+
+    /** Logo should be exisiting and its for desktop and mobile = 2 */
+    it("should contain flenzr logo", async () => {
+      render(<FeatureProductPage navigateTo={mockNavigateTo} />);
+      const logos = await screen.findAllByAltText("Flenzr");
+      expect(logos).toBeDefined();
+      expect(logos.length).toBe(2);
+    });
+
+    /** should have signUp Button */
+    it("Should have login and sign up buttons", async () => {
+      render(<FeatureProductPage navigateTo={mockNavigateTo} />);
+      const { t } = useTranslation();
+      const signUpBtn = await screen.findByText(t("join"));
+      console.log(signUpBtn);
+      expect(signUpBtn).toBeDefined();
+    });
   });
+
+  /** Unit test cases for testing the logic */
+  describe("Test cases for FeatureProductPage business logic", () => {
+    it("Should Route emit SignUp", async() => {
+      render(<FeatureProductPage navigateTo={mockNavigateTo} />);
+      const { t } = useTranslation();
+      const signUpBtn = await screen.findByText(t("join"));
+      fireEvent.click(signUpBtn);
+      expect(mockNavigateTo).toHaveBeenCalledWith("signup")
+    })
+
+    it("Should Route emit Signin", async() => {
+      render(<FeatureProductPage navigateTo={mockNavigateTo} />);
+      const { t } = useTranslation();
+      const signUpBtn = await screen.findByText(t("login"));
+      fireEvent.click(signUpBtn);
+      expect(mockNavigateTo).toHaveBeenCalledWith("signin")
+    })
+  })
 });
