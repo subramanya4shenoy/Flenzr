1818ad6078147e46e7f1e7f28ff5fecb8634cd82
subu <subramanya4shenoy@gmail.com>
2022-08-11 02:05:51 +0530

    menu bar

--- libs/shared/ui-menu-bar/src/lib/shared-ui-menu-bar.tsx
+++ libs/shared/ui-menu-bar/src/lib/shared-ui-menu-bar.tsx
@@ -1,26 +1,26 @@
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
+import { SyntheticEvent, useState } from "react";
+import { useTranslation } from "react-i18next";
 
+export function SharedUiMenuBar() {
+  const [value, setValue] = useState(0);
+  const { t } = useTranslation();
 
-
-
-
-  import styles from './shared-ui-menu-bar.module.scss';
-  
-
-/* eslint-disable-next-line */
-export interface SharedUiMenuBarProps {
-}
-
-
-
-export function SharedUiMenuBar(props: SharedUiMenuBarProps) {
+  const handleChange = (event: SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
   return (
-    <div className={styles['container']}>
-      
-      <h1>Welcome to SharedUiMenuBar!</h1>
-      
+    <div className="shadow-md w-auto mx-auto max-w-xl rounded-full -mt-6 bg-white">
+      <Tabs value={value} onChange={handleChange} centered>
+        <Tab label={t("feeds")} />
+        <Tab label={t("about")} />
+        <Tab label={t("community")} />
+        <Tab label={t("brands")} />
+        <Tab label={t("connections")} />
+      </Tabs>
     </div>
   );
-};
-
+}
 
 export default SharedUiMenuBar;
