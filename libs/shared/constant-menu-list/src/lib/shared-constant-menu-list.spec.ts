97525281824ae8c4a34941d9e0af472a0a0a6819
Subu <subramanya4shenoy@gmail.com>
2022-08-12 02:17:28 +0530

    Menu navigations (#18)
    * dashboard view centered
    
    * common menu list and dynamic views

---
+++ libs/shared/constant-menu-list/src/lib/shared-constant-menu-list.spec.ts
@@ -0,0 +1,7 @@
+import { sharedConstantMenuList } from './shared-constant-menu-list';
+
+describe('sharedConstantMenuList', () => {
+    it('should work', () => {
+        expect(sharedConstantMenuList()).toEqual('shared-constant-menu-list');
+    })
+})
--- libs/shared/ui-menu-bar/src/lib/shared-ui-menu-bar.tsx
+++ libs/shared/ui-menu-bar/src/lib/shared-ui-menu-bar.tsx
@@ -1,26 +1,25 @@
 import Tab from "@mui/material/Tab";
 import Tabs from "@mui/material/Tabs";
-import { SyntheticEvent, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { MENU_LIST } from "@flenzr/shared/constant-menu-list";
 
-export function SharedUiMenuBar() {
-  const [value, setValue] = useState(0);
+export interface SharedUiMenuBarProps {
+  selectedMenu: {index:number; id: string; label: string; view: string},
+  onMenuChange(val:{ index: number; id: string; label: string; view: string}):void
+}
+
+export const SharedUiMenuBar = ({selectedMenu, onMenuChange}:SharedUiMenuBarProps) => {
+
   const { t } = useTranslation();
 
-  const handleChange = (event: SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
   return (
     <div className="shadow-md w-auto mx-auto max-w-xl rounded-full -mt-6 bg-white">
-      <Tabs value={value} onChange={handleChange} centered>
-        <Tab label={t("feeds")} />
-        <Tab label={t("about")} />
-        <Tab label={t("community")} />
-        <Tab label={t("brands")} />
-        <Tab label={t("connections")} />
+      <Tabs value={selectedMenu.index} 
+      onChange={(e, val) => {onMenuChange(MENU_LIST[val])}} centered>
+        {MENU_LIST.map((menu) => {
+          return <Tab key={menu.id} label={t(menu.label)} />;
+        })}
       </Tabs>
     </div>
   );
 }
-
-export default SharedUiMenuBar;
