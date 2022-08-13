386dbc85c79e9078f442a1a99eef004609db7bcd
Subu <subramanya4shenoy@gmail.com>
2022-08-14 01:25:49 +0530

    Connections (#21)
    * connection card
    
    * user card and unfollow button
    
    * moved shared ui unfollow to feature unfollow
    
    * social list

---
+++ libs/feature/social-list/README.md
@@ -0,0 +1,7 @@
+# feature-social-list
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test feature-social-list` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/feature/social-unfollow/src/lib/shared-social-unfollow.tsx
@@ -0,0 +1,10 @@
+import Button from '@mui/material/Button';
+
+export function SharedSocialUnfollow() {
+  return (
+    <Button variant='contained' color="error" fullWidth={true}>unfollow</Button>
+  );
+};
+
+
+export default SharedSocialUnfollow;
