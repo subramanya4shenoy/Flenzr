aadc57946a2c86599e9000343907b2c57db9faab
subu <subramanya4shenoy@gmail.com>
2022-08-13 23:35:15 +0530

    user card and unfollow button

---
+++ libs/shared/social-unfollow/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'shared-social-unfollow',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/shared/social-unfollow'
+};
---
+++ libs/shared/social-unfollow/src/lib/shared-social-unfollow.tsx
@@ -0,0 +1,10 @@
+import Button from '@mui/material/Button';
+
+export function SharedSocialUnfollow() {
+  return (
+    <Button variant='contained' color="error">unfollow</Button>
+  );
+};
+
+
+export default SharedSocialUnfollow;
