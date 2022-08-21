05a59abf5badb4f1747f7caaafecc4fea64c4c77
subu <subramanya4shenoy@gmail.com>
2022-08-22 00:02:16 +0530

    left rail section

--- libs/feature/profile-feeds/src/lib/feature-profile-feeds.tsx
+++ libs/feature/profile-feeds/src/lib/feature-profile-feeds.tsx
@@ -1,19 +1,23 @@
 import { FeatureStatsCard } from "@flenzr/feature/stats-card";
 import { FeatureProfileCategory } from "@flenzr/feature/profile-category";
 import { SharedUiRating } from "@flenzr/shared/ui-rating";
+import { FeatureProfileTargetLocation } from "@flenzr/feature/profile-target-location";
 
 export function FeatureProfileFeeds() {
   return (
     <div className="flex">
-      <div className="w-2/3">flowing feed</div>
       <div className="w-1/3">
         <div className="mb-4 flex justify-center items-center">
           <div>
             <FeatureStatsCard />
             <FeatureProfileCategory/>
             <SharedUiRating/>
+            <FeatureProfileTargetLocation/>
           </div>
         </div>
+      </div>
+      <div className="w-2/3">
+        right
       </div>
     </div>
   );
