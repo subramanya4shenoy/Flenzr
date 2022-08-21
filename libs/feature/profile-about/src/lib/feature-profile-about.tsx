ea040ec412a810c469837b713c6d802fecf53bd8
subu <subramanya4shenoy@gmail.com>
2022-08-21 20:43:47 +0530

    About

--- libs/feature/profile-about/src/lib/feature-profile-about.tsx
+++ libs/feature/profile-about/src/lib/feature-profile-about.tsx
@@ -1,26 +1,33 @@
-
-
-
-
-
-  import styles from './feature-profile-about.module.scss';
-  
+import Typography from "@mui/material/Typography";
+import { useTranslation } from "react-i18next";
 
 /* eslint-disable-next-line */
-export interface FeatureProfileAboutProps {
-}
-
-
+export interface FeatureProfileAboutProps {}
 
 export function FeatureProfileAbout(props: FeatureProfileAboutProps) {
+  const { t } = useTranslation();
+
   return (
-    <div className={styles['container']}>
-      
-      <h1>Welcome to FeatureProfileAbout!</h1>
-      
+    <div className="flex justify-center content-center items-top">
+      <div className="m-4 p-6 shadow rounded-lg w-1/2">
+        <Typography variant="h6" gutterBottom>
+          {t("about")}
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error magnam
+          amet, harum, vitae laboriosam quis iusto, fuga commodi suscipit eius
+          qui et odit. Nam ea quasi sapiente, numquam quia cupiditate.
+        </Typography>
+      </div>
+
+      <div className="m-4 p-6 shadow rounded-lg w1/2">
+        <Typography variant="h6" gutterBottom>
+          {t("Worked with Brands")}
+        </Typography>
+        list
+      </div>
     </div>
   );
-};
-
+}
 
 export default FeatureProfileAbout;
