74f0d7c8cf1c37c7798f1a189f71dd6f9abddecb
Subu <subramanya4shenoy@gmail.com>
2022-08-23 22:13:47 +0530

    Feed (#32)
    * feed page basic
    
    * post component

--- libs/feature/user-post/src/lib/feature-user-post.tsx
+++ libs/feature/user-post/src/lib/feature-user-post.tsx
@@ -1,26 +1,54 @@
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import { useTranslation } from "react-i18next";
 
+export function FeatureUserPost() {
+  const { t } = useTranslation();
 
-
-
-
-  import styles from './feature-user-post.module.scss';
-  
-
-/* eslint-disable-next-line */
-export interface FeatureUserPostProps {
-}
-
-
-
-export function FeatureUserPost(props: FeatureUserPostProps) {
   return (
-    <div className={styles['container']}>
-      
-      <h1>Welcome to FeatureUserPost!</h1>
-      
+    <div className="w-full mx-4 p-5 bg-white rounded-lg shadow-md">
+      <div>{t("whatAreYouUpto")}</div>
+      <div>
+        <TextField
+          color="primary"
+          autoFocus={true}
+          type="text"
+          margin="normal"
+          multiline
+          rows={4}
+          sx={{
+            background: "#F2F4F8",
+            borderColor: "#F2F4F8",
+            borderRadius: "16px",
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                borderColor: "transparent",
+              },
+              "&:hover fieldset": {
+                borderColor: "transparent",
+              },
+              "& .MuiOutlinedInput-root": {
+                "& fieldset": {
+                  borderColor: "transparent",
+                },
+                "&:hover fieldset": {
+                  borderColor: "transparent",
+                },
+                "&.Mui-focused fieldset": {
+                  borderColor: "transparent",
+                },
+              },
+            },
+          }}
+          className="w-full"
+        />
+      </div>
+      <div className="flex justify-end">
+        <Button className="float-right" variant="contained">Share</Button>
+      </div>
+
     </div>
   );
-};
-
+}
 
 export default FeatureUserPost;
