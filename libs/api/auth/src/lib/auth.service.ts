7fb0e6c6e35fd0b3c44b73360a8850be13726c5a
subu <subramanya4shenoy@gmail.com>
2022-09-17 16:02:50 +0530

    fix(code cleanup): code clean up in backend
    Weekly code clean up session

--- libs/api/auth/src/lib/auth.service.ts
+++ libs/api/auth/src/lib/auth.service.ts
@@ -3,43 +3,53 @@ import { AuthSignInInput } from "./dto/auth-signin.input";
 import { AuthSignUpInput } from "./dto/auth-signup.input";
 import { UserToken } from "./models/user-token";
 import { PrismaService } from "./prisma.service";
-import * as bcrypt from 'bcrypt';
 import { JwtService } from "@nestjs/jwt";
-import * as moment from 'moment';
+import * as bcrypt from "bcrypt";
+import * as moment from "moment";
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService, private jwtService: JwtService) {}
-  
+
+  /**
+   * @name SignIn logic
+   * @desc if user found login and update signin activity
+   */
   async signIn(input: AuthSignInInput): Promise<UserToken> {
     const user = await this.prisma.user.findFirst({
-      where: {
-        email: input.email,
-      },
+      where: { email: input.email },
     });
 
+    /** If user found update signin activity and send token and userInfo */
     if (user) {
-      await this.prisma.user_signin_activity.update({
-        where: {
-        user_id: user.user_id,
-        },
-        data: {
-          last_login_time: moment().format("D-MMM-YY hh.mm.ss.SSSSSS A Z")
-        }
-      })
+      await this.updateUserLoginActivity(user, false);
       const isMatch = await bcrypt.compare(input.password, user.password);
-      if(isMatch) {
-        return {token: this.generateToken(user), user: user};
-      } else { this.comonError("Password miss-match"); }
+      if (isMatch) {
+        return { token: this.generateToken(user), user: user };
+      } else {
+        this.comonError("Password miss-match");
+      }
     }
+
     if (!user) {
-      this.comonError("This email address is does not exist. Please try signing up");
+      this.comonError(
+        "This email address is does not exist. Please try signing up"
+      );
     }
   }
 
 
+
+
+
+  /**
+   * @param input 
+   * @returns user Object and token
+   * @desc sign up the user with verfified as false, 
+   * crete the signInActivityDB entry.
+   * @pending [send email to verify] 
+   */
   async signUp(input: AuthSignUpInput): Promise<UserToken> {
-    
-    if(input.email == '' || input.password == '') {
+    if (input.email == "" || input.password == "") {
       this.comonError("Please fill valid email and password");
     }
     const existingUser = await this.prisma.user.findFirst({
@@ -47,11 +57,13 @@ export class AuthService {
         email: input.email,
       },
     });
-    
-    if (existingUser) { 
-      this.comonError("This email address is already registered. Please try Loging In");
+
+    if (existingUser) {
+      this.comonError(
+        "This email address is already registered. Please try Loging In"
+      );
     } else {
-      const salt = 10
+      const salt = 10;
       const hash = await bcrypt.hash(input.password, salt);
       input.password = hash;
       const newUser = await this.prisma.user.create({
@@ -59,21 +71,59 @@ export class AuthService {
           ...input,
         },
       });
-      await this.prisma.user_signin_activity.create({
-        data: {
-          user_id: newUser.user_id,
-          last_login_time:  moment().format("D-MMM-YY hh.mm.ss.SSSSSS A Z"),
-          location: input.location || ''
-        }
-      })
-      return {token: this.generateToken(newUser), user: newUser};
+      await this.updateUserLoginActivity(newUser, true);
+      return { token: this.generateToken(newUser), user: newUser };
     }
   }
 
-  generateToken(payload):string {
+
+
+
+
+
+  /**
+   * 
+   * @param user false for already exisitng user
+   * @param isNewuser true id triggered while signup
+   */
+  async updateUserLoginActivity(user, isNewuser?) {
+    if (!isNewuser) {
+      await this.prisma.user_signin_activity.update({
+        where: {
+          user_id: user.user_id,
+        },
+        data: {
+          last_login_time: moment().format("D-MMM-YY hh.mm.ss.SSSSSS A Z"),
+          location: user.location || "",
+        },
+      });
+    } else {
+      await this.prisma.user_signin_activity.create({
+        data: {
+          user_id: user.user_id,
+          last_login_time: moment().format("D-MMM-YY hh.mm.ss.SSSSSS A Z"),
+          location: user.location || "",
+        },
+      });
+    }
+  }
+
+  /**
+   * @desc logic to generate jwt token
+   * @param payload 
+   * @returns 
+   */
+  generateToken(payload): string {
     return this.jwtService.sign(payload);
   }
 
+  
+  /**
+   * @desc using default forbidden error
+   * @param msg 
+   * @pending need to handle the error types properly and
+   * avoid common forbidden method.
+   */
   comonError(msg: string): void {
     throw new ForbiddenException(msg);
   }
