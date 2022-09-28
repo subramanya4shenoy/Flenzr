d0b83a5e098c5c0d4a791149666b8908c10fb988
subu <subramanya4shenoy@gmail.com>
2022-09-28 23:51:57 +0530

    refactor(moving the appid to env and refactoring the component): facebook signin
    login via sign in

--- libs/shared/ui-third-party-signin/src/lib/facebook-signin/facebook-signin.tsx
+++ libs/shared/ui-third-party-signin/src/lib/facebook-signin/facebook-signin.tsx
@@ -1,19 +1,19 @@
 import IconButton from "@mui/material/IconButton";
 import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
-import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
 import { useState } from "react";
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
 
 const FacebookSignIn = ({ onSuccess }: any) => {
 
   return (
     <FacebookLogin
-      appId="123"
-      autoLoad={true}
+      appId= { process.env["NX_FACEBOOK_APP_ID"] as string }
+      autoLoad={false}
       fields="name,email,picture"
       onClick={(data)=>{console.log("clicked", data)}}
       callback={(data) => { console.log("data",data)}}
       onFailure={(data) => { console.log("error",data)}}
-      render={({onClick}) => {
+      render={({onClick}: any) => {
         return <IconButton onClick={onClick} aria-label="Facebook">
                 <FacebookRoundedIcon />
                </IconButton>;
