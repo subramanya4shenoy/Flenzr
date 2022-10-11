dd564103cc5d59215953e0a1d507ad3fb8b5bc07
subu <subramanya4shenoy@gmail.com>
2022-10-11 23:15:06 +0530

    feat(my social work in progress): my social integration with stepper and components

---
+++ libs/feature/social-stepper/src/lib/feature-social-stepper.constant.ts
@@ -0,0 +1,18 @@
+export const customFunction = (cb:any) => {
+    cb();
+}
+
+export const MY_SOCIAL_STEPS = [
+    {
+        stepTitleKey: 'addYourSocialAccounts',
+        stepSubTitleKey: ''
+    },
+    {
+        stepTitleKey: 'addanEpicIntro',
+        stepSubTitleKey: ''
+    },
+    {
+        stepTitleKey: 'setYourStyleOfContent',
+        stepSubTitleKey: ''
+    }
+];
