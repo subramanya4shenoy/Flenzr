a819ab7798650464482e4fa3dbef25f9c3ec1f7d
Subu <subramanya4shenoy@gmail.com>
2022-10-12 23:23:09 +0530

    Social add platform (#79)
    * feat(my social work in progress): my social integration with stepper and components
    
    * feat(social platforms): adding social platforms

--- libs/shared/social-add-about/src/lib/shared-social-add-about.tsx
+++ libs/shared/social-add-about/src/lib/shared-social-add-about.tsx
@@ -1,23 +1,46 @@
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
 
-
-
-
-
-  import styles from './shared-social-add-about.module.scss';
-  
-
-/* eslint-disable-next-line */
-export interface SharedSocialAddAboutProps {
-}
-
-
-
-export function SharedSocialAddAbout(props: SharedSocialAddAboutProps) {
+export function SharedSocialAddAbout() {
+  const [about, setAbout] = useState("");
+  const { t } = useTranslation();
   return (
-    <div className={styles['container']}>
-      
-      <h1>Welcome to SharedSocialAddAbout!</h1>
-      
+    <div>
+      <TextField
+            color="primary"
+            autoFocus={true}
+            id="about"
+            multiline
+            maxRows={4}
+            value={about}
+            type="text"
+            margin="normal"
+            sx={{
+              background: "#F2F4F8",
+              borderColor: "#F2F4F8",
+              borderRadius: "16px",
+              "& .MuiOutlinedInput-root": {
+                "& fieldset": {
+                  borderColor: "transparent",
+                },
+                "&:hover fieldset": {
+                  borderColor: "transparent",
+                }
+              },
+            }}
+            className="w-full"
+            onChange={(e) => setAbout(e.target.value)}
+          />
+      <div className='flex justify-end'>
+    <Button sx={{mr: 1}} 
+            variant="contained"
+            color="primary"
+            onClick={() => {console.log("submit")}}>
+              {t('update')}
+    </Button>
+  </div>
     </div>
   );
 };
---
+++ libs/shared/social-add-platform/src/lib/socialPannels/social-add-fb.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SocialAddFb = () => {
+  return (
+    <div>SocialAddFb</div>
+  )
+}
+
+export default SocialAddFb
