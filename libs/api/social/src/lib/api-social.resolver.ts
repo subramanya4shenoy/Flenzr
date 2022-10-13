a7b056d0d751c3faced2eb2eaa36563ff0056894
subu <subramanya4shenoy@gmail.com>
2022-10-14 00:15:47 +0530

    fix(guard setup): guard setup
    Guard setup

--- libs/api/social/src/lib/api-social.resolver.ts
+++ libs/api/social/src/lib/api-social.resolver.ts
@@ -1,10 +1,13 @@
+import { UseGuards } from "@nestjs/common";
 import { Query, Resolver } from "@nestjs/graphql";
+import { GqlAuthGuard } from "./guard/auth/jwt-auth.guard";
 
 @Resolver()
 export class SocialResolver {
 
     // Fetch all youtube channels of user
     @Query(() => String)
+    @UseGuards(GqlAuthGuard)
     fetchAllYtChannels(): string {
         return "ytChannels boi!"
     }
