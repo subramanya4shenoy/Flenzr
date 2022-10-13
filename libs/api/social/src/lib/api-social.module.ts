5b169021d18b57d1a8d149e2404e5fc117bfc70c
Subu <subramanya4shenoy@gmail.com>
2022-10-14 00:19:25 +0530

    Social add platform2 (#81)
    * feat(social backend services): social backend services
    
    * fix(guard setup): guard setup
    
    Guard setup

--- libs/api/social/src/lib/api-social.module.ts
+++ libs/api/social/src/lib/api-social.module.ts
@@ -1,12 +1,14 @@
 import { ApiAuthModule } from '@flenzr/api/auth';
 import { Module } from '@nestjs/common';
 import { SocialResolver } from './api-social.resolver';
+import { GqlAuthGuard } from './guard/auth/jwt-auth.guard';
 
 @Module({
 	imports: [ApiAuthModule],
 	controllers: [],
 	providers: [
-		SocialResolver
+		SocialResolver,
+		GqlAuthGuard
 	],
 	exports: [],
 })
