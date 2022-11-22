f49a8cc1af31367bc6719025a60494d7a2df8485
Subu <subramanya4shenoy@gmail.com>
2022-11-22 22:19:50 +0530

    Update release.sh

--- scripts/pipeline/release.sh
+++ scripts/pipeline/release.sh
@@ -9,6 +9,7 @@ local_path=$5
 
 # Create a zip of the current directory.
 #zip -r $local_path . -x .git/ .git/*** .github/workflows/release.yml scripts/pipeline/release.sh scripts/pipeline/upload_file_to_s3.py .DS_Store
+zip -r shell.zip local_path
 
 # Install required dependencies for Python script.
 pip3 install boto3
