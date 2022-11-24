ccb2fb86ad3bf4a0ed0cf12b233e73f80dd1ac39
Subu <subramanya4shenoy@gmail.com>
2022-11-24 23:00:33 +0530

    Update release.sh

--- scripts/pipeline/release.sh
+++ scripts/pipeline/release.sh
@@ -16,24 +16,11 @@ local_path=$5
 
 # Create a zip of the current directory.
 pushd $local_path
-echo "PWD PRE =>"
-pwd
-echo "PREVIOUS FILES =>"
-ls -ltar
 zip -r $aws_key ./*
-echo "PWD POST =>"
-pwd
-echo "POST FILES =>"
-ls -ltar
 popd
-
-echo "PWD after popd =>"
-pwd
-echo "after popd FILES =>"
-ls -ltar
 
 # Install required dependencies for Python script.
 pip3 install boto3
 
 # Run upload script
-# python3 scripts/pipeline/upload_file_to_s3.py $bucket_name $aws_key $aws_access_key $aws_access_secret $aws_key
+python3 scripts/pipeline/upload_file_to_s3.py $bucket_name $aws_key $aws_access_key $aws_access_secret $local_path/$aws_key
