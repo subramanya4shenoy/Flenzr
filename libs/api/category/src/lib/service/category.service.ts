cc92935d17d74bc17d6d4a9aeff4dd3206619a6c
Subu <subramanya4shenoy@gmail.com>
2023-01-29 21:08:24 +0530

    Category setup (#110)
    * feat(backend changes for category): backend changes for CAtegory and library created
    
    * feat(category section updation): cAtegory section updation
    
    CAtegory section updation
    
    * feat: category and subcategory setup

--- libs/api/category/src/lib/service/category.service.ts
+++ libs/api/category/src/lib/service/category.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from "@nestjs/common";
 import { CATEGORIES } from "../constant/category.constant";
-import { CatgoryModel } from '../models/category.model';
+import { CategoryModel } from '../models/category.model';
 @Injectable()
 export class CategoryService {
-    async getAllCategories(): Promise<CatgoryModel[]> {
-        return await CATEGORIES;
-      }
+  async getAllCategories(): Promise<CategoryModel[]> {
+    return await CATEGORIES;
+  }
+
+  async saveCategory(category: CategoryModel): Promise<CategoryModel> {
+    CATEGORIES.push(category);
+    return category;
+  }
 }
